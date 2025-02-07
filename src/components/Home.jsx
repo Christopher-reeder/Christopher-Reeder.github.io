ae67f32afc018dcb7e5f00e5b3a8140aae99dc77
Chris <syhbstk@gmail.com>
2025-02-07 14:35:21 -0800

    modified index.css + add welcoming message for home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import Navbar from './Navbar';
 import './Home.css';
+import { useNavigate } from 'react-router-dom';
 
-export default function Home() {
-  return (
-    <div>
-      <Navbar />
-      <h2>
-        11111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111
-      </h2>
-    </div>
-  );
+
+function Home() {
+    return (
+      <div>
+        <Navbar />
+        <section style={{ padding: '2rem', textAlign: 'center' }}>
+          <h1>Welcome to the Website!</h1>
+          <p>
+            Dive into the worlds of programes and drawings created by Chris Reeder. <br />
+            What would you like to explore?
+          </p>
+        </section>
+      </div>
+  );    
 }
+
+export default Home;
