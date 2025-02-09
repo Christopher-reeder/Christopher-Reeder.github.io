89c3ed222837f5f1b36c4af5675b2d32c12e8eec
Chris <syhbstk@gmail.com>
2025-02-08 23:05:40 -0800

    implement array and func for art works (no descriptions yet)

--- src/components/Drawing.jsx
+++ src/components/Drawing.jsx
@@ -3,9 +3,45 @@ import Navbar from './Navbar';
 import './Drawing.css';
 
 function Drawing() {
+  const artworks = [
+    {
+      imageSrc: '111',
+      alt: '1',
+      description: `
+      1`,
+    },
+    {
+      imageSrc: '222',
+      alt: '2',
+      description: `
+      2`,
+    },
+    {
+      imageSrc: '333',
+      alt: '3',
+      description: `
+      3`,      
+    },
+  ];
+
+  function getArtwork(art, index) {
+    return (
+      <div key={index} className="artwork-item">
+        <div className="artwork-image">
+          <img src={art.imageSrc} alt={art.alt} />
+        </div>
+          
+        <div className="artwork-description">
+          <p>{art.description}</p>
+        </div>
+      </div>
+    )
+  }
+
   return (
     <div>
       <Navbar />
+
       <div className="sns-links">
         <h2>My Art Portfolio</h2>
         <div className="sns-buttons">
@@ -25,7 +61,9 @@ function Drawing() {
         <h3>
           {`I enjoy drawing fan art for games and anime, but what I love most is creating concept art for the original stories I’ve envisioned. 
           Below are the three projects I have developed and illustrated the most.`}
-        </h3>
+        </h3>     
+
+        {artworks.map(getArtwork)}   
       </div>
       
     </div>
