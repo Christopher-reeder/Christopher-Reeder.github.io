bb5a45915c627b2a48cc2c5c972e0c68f574bfaf
Chris <syhbstk@gmail.com>
2025-02-08 22:45:35 -0800

    design introduction part

--- src/components/Drawing.jsx
+++ src/components/Drawing.jsx
@@ -1,13 +1,35 @@
-import React from "react";
-import "./Drawing.css";
+import React from 'react';
 import Navbar from './Navbar';
+import './Drawing.css';
 
-export default function Drawing() {
+function Drawing() {
   return (
     <div>
-      <h1>
-        Drawing
-      </h1>
+      <Navbar />
+      <div className="sns-links">
+        <h2>My Art Portfolio</h2>
+        <div className="sns-buttons">
+          <a>
+            Instagram
+          </a>
+          <a>
+            Pixiv
+          </a>
+          <a>
+            ArtStation
+          </a>
+        </div>
+      </div>
+
+      <div className="artworks">
+        <h3>
+          {`I enjoy drawing fan art for games and anime, but what I love most is creating concept art for the original stories I’ve envisioned. 
+          Below are the three projects I have developed and illustrated the most.`}
+        </h3>
+      </div>
+      
     </div>
   );
 }
+
+export default Drawing;
