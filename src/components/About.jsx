5169076a592f3917433bd868167f3e338c7f6a15
Chris <syhbstk@gmail.com>
2025-02-08 17:48:57 -0800

    add image + make frame for text

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,9 +5,32 @@ import Navbar from './Navbar';
 export default function About() {
   return (
     <div>
-      <h1>
-        about
-      </h1>
+      <Navbar />
+      <div className="about-top-container">
+        <div className="about-image">
+          <img
+            src="/images/face.png"
+          />
+        </div>
+
+        <div>
+          <h1>Christopher Kade Reeder</h1>
+          <p>
+            welcome
+          </p>
+
+          <h2>Skills & Tools</h2>
+          <ul>
+            123
+          </ul>
+        </div>
+      </div>
+      <div className="about-bottom-container">
+        <h2>Values</h2>
+        <p>
+          123
+        </p>
+      </div>
     </div>
   );
 }
